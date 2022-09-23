63bdaa23636d77c4b3df5e7377c89e724a1d28d4
oussama <fzoussfr@gmail.com>
2022-09-23 14:04:58 +0100

    modified the menu page and created add information page without structure

--- fonction1.h
+++ fonction1.h
@@ -1,7 +1,46 @@
 #ifndef FONCTION1_H_INCLUDED
 #define FONCTION1_H_INCLUDED
-#endif  
+
 void fonction1()
-{
-    printf("hello world");
+{  char pname[100],disease[100];
+   int entries,age,cabin_no,phone,choice;
+    FILE* fichier = NULL;
+    fichier = fopen("patients", "w");
+
+    printf("\t\tHow many entry do you want to add ?");
+    scanf("%d",&entries);
+
+    printf("\tEnter patient's name:");
+    scanf("%s",pname);
+    fprintf(fichier, "patient's name : %s\n",pname);
+
+    printf("\tEnter patient's disease:");
+    scanf("%s",disease);
+    fprintf(fichier, "patient's disease : %s\n",disease);
+
+    printf("\tEnter the age:");
+    scanf("%d",&age);
+    fprintf(fichier, "age : %d\n",age);
+
+    printf("\tEnter cabin no:");
+    scanf("%d",&cabin_no);
+    fprintf(fichier, "cabin number : %d\n",cabin_no);
+
+    printf("\tEnter phone number:");
+    scanf("%d",&phone);
+    fprintf(fichier, "full name : %d\n",phone);
+
+
+    fclose(fichier);
+    printf("\n\n");
+    printf("\tEnter 1 to go to the main menu or 0 to exit:");
+    scanf("%d",&choice);
+
+    if(choice==0)
+        system("exit");
+    else
+        {   system("cls");
+            main();
+        }
 }
+#endif
--- main.c
+++ main.c
@@ -3,21 +3,21 @@
 #include<windows.h>
 #include"fonction1.h"
 int main()
-{   
-   int opt; 
-    printf("\t\tHOSPITAL MANAGEMENT SYSTEM");
-    printf("\t\t***********************\n\t\t***********************\n\t\t***********************");
-    printf("\t\t**Enter your choice");
-    printf("\t\t1.Add information:");
-    printf("\t\t2.View information:");
-    printf("\t\t3.Search:");
-    printf("\t\t4.Edit information:");
-    printf("\t\t5.Exit");
+{
+   int opt;
+    printf("\n\n\t\tHOSPITAL MANAGEMENT SYSTEM");
+    printf("\n\t\t***********************\n\t\t***********************\n\t\t***********************\n\n");
+    printf("\t\tEnter your choice\n");
+    printf("\t\t1.Add information:\n");
+    printf("\t\t2.View information:\n");
+    printf("\t\t3.Search:\n");
+    printf("\t\t4.Edit information:\n");
+    printf("\t\t5.Exit\n");
     scanf("%d",&opt);
     switch(opt)
     {
         case 1:
-        {   
+        {
             system("cls");
             fonction1();
             break;
